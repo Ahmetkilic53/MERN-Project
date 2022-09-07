e4368d4d0e09e5498923bb924aa943a7d75ac488
Muhammet Ahmet KILIÇ <86723225+Ahmetkilic53@users.noreply.github.com>
2022-09-07 15:23:58 +0300

    Mern Project
    x

---
+++ mern/server/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose, { model } from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userType: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER'
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+})
+
+export default mongoosei.model('User', userSchema)
